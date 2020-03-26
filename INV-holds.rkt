4be454116c7210b085142a3b8ff75ba1cb2af7d2
Isabella C Felix <felixisa@shu.edu>
2020-03-25 22:07:48 -0400

    added TEST-MACHINE

--- INV-holds.rkt
+++ INV-holds.rkt
@@ -23,19 +23,23 @@
 ; (sm-showtransitions EVEN-NUM-B '())
 ; '((() Q0) accept)
 
-; (sm-showtransitions EVEN-NUM-B '(a))
-; '(((a) Q0) (() Q0) accept)
-
-; (sm-showtransitions EVEN-NUM-B '(b a))
-; '(((b a) Q0) ((a) Q1) (() Q1) reject)
-
-; (sm-showtransitions EVEN-NUM-B '(b b))
-; '(((b b) Q0) ((b) Q1) (() Q0) accept)
-
 (define (INVS-HOLD-e)
   (if (Q0-INV '())
       #t
       (error (format "Q0-INV failed for ~s" '()))))
+
+; (sm-showtransitions EVEN-NUM-B '(a))
+; '(((a) Q0) (() Q0) accept)
+
+(define (INV-HOLDS-a)
+  (cond [(not (Q0-INV '()))
+         (error (format "Q0-INV failed for ~s" '()))]
+        [(not (Q0-INV '(a)))
+         (error (format "Q0-INV failed for ~s" '(a)))]
+        [else #t]))
+
+; (sm-showtransitions EVEN-NUM-B '(b a))
+; '(((b a) Q0) ((a) Q1) (() Q1) reject)
 
 (define (INVS-HOLD-ba)
   (cond [(not (Q0-INV '()))
@@ -44,6 +48,125 @@
          (error (format "Q0-INV failed for ~s" '(b)))]
         [(not (Q1-INV '(b a)))
          (error (format "Q0-INV failed for ~s" '(b a)))]
-        [else #t])) 
+        [else #t]))
+
+; (sm-showtransitions EVEN-NUM-B '(b b))
+; '(((b b) Q0) ((b) Q1) (() Q0) accept)
+
+(define (INVS-HOLD-bb)
+  (cond [(not (Q0-INV '()))
+         (error (format "Q0-INV failed for ~s" '()))]
+        [(not (Q1-INV '(b)))
+         (error (format "Q0-INV failed for ~s" '(b)))]
+        [(not (Q0-INV '(b b)))
+         (error (format "Q0-INV failed for ~s" '(b b)))]
+        [else #t]))
+
+
+(define TEST-MACHINE
+  (make-dfa
+   '(A B C D)
+   '(a b)
+   'A
+   '(C)
+   '((A a B)
+     (A b D)
+     (B a A)
+     (B b C)
+     (D a D)
+     (D b C)
+     (C a C)
+     (C b C))))
+
+(define (A-INV ci)
+  (even? (length (filter (lambda (i) (equal? i 'a)) ci))))
+
+(define (B-INV ci)
+  (and (andmap (lambda (i) (equal? i 'a)) ci)
+       (odd? (length (filter (lambda (i) (equal? i 'a)) ci))))) 
+
+(define (C-INV ci)
+  (and (>= (length ci))
+       (or (equal? (take-right ci 1) '(a))
+           (equal? (take-right ci 1) '(b)))))
+
+(define (D-INV ci)
+  (or (and (= (length ci) 1)
+           (equal? (car ci) 'b))
+      (and (= (length (filter (lambda (i) (equal? i 'b)) ci)) 1)
+           (equal? (take-right ci 1) '(a)))))
+
+; '(() (a a) (b a) (b b) (a b a) (a b b))
+
+; (sm-showtransitions TEST-MACHINE '())
+; '((() A) reject)
+
+(define (INV-HOLDS-empty)
+  (if (A-INV '())
+      #t
+      (error (format "A-INV failed for ~s" '()))))
+
+; (sm-showtransitions TEST-MACHINE '(a a))
+; '(((a a) A) ((a) B) (() A) reject)
+
+(define (INVS-HOLD-aa)
+  (cond [(not (A-INV '()))
+         (error (format "A-INV failed for ~s" '()))]
+        [(not (B-INV '(a)))
+         (error (format "B-INV failed for ~s" '(a)))]
+        [(not (A-INV '(a a)))
+         (error (format "A-INV failed for ~s" '(a a)))]
+        [else #t]))
+
+; (sm-showtransitions TEST-MACHINE '(b a))
+; '(((b a) A) ((a) D) (() D) reject)
+
+(define (INVS-HOLD-ba2)
+  (cond [(not (A-INV '()))
+         (error (format "A-INV failed for ~s" '()))]
+        [(not (D-INV '(b)))
+         (error (format "A-INV failed for ~s" '(b)))]
+        [(not (D-INV '(b a)))
+         (error (format "D-INV failed for ~s" '(b a)))]
+        [else #t]))
+
+; (sm-showtransitions TEST-MACHINE '(b b))
+; '(((b b) A) ((b) D) (() C) accept)
+
+(define (INVS-HOLD-bb2)
+  (cond [(not (A-INV '()))
+         (error (format "A-INV failed for ~s" '()))]
+        [(not (D-INV '(b)))
+         (error (format "A-INV failed for ~s" '(b)))]
+        [(not (C-INV '(b b)))
+         (error (format "C-INV failed for ~s" '(b b)))]
+        [else #t]))
+
+; (sm-showtransitions TEST-MACHINE '(a b a))
+; '(((a b a) A) ((b a) B) ((a) C) (() C) accept)
+
+(define (INVS-HOLD-aba)
+  (cond [(not (A-INV '()))
+         (error (format "A-INV failed for ~s" '()))]
+        [(not (B-INV '(a)))
+         (error (format "B-INV failed for ~s" '(a)))]
+        [(not (C-INV '(a b)))
+         (error (format "C-INV failed for ~s" '(a b)))]
+        [(not (C-INV '(a b a)))
+         (error (format "C-INV failed for ~s" '(a b a)))]
+        [else #t]))
+
+; (sm-showtransitions TEST-MACHINE '(a b b))
+; '(((a b b) A) ((b b) B) ((b) C) (() C) accept)
+(define (INVS-HOLD-abb)
+  (cond [(not (A-INV '()))
+         (error (format "A-INV failed for ~s" '()))]
+        [(not (B-INV '(a)))
+         (error (format "B-INV failed for ~s" '(a)))]
+        [(not (C-INV '(a b)))
+         (error (format "C-INV failed for ~s" '(a b)))]
+        [(not (C-INV '(a b b)))
+         (error (format "C-INV failed for ~s" '(a b b)))]
+        [else #t]))
 
 (test) 
