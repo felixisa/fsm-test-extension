655b0f1cc0d499c46f9c6e72ef457a23a9c843f5
Sena N Karsavran <karsavse@shu.edu>
2020-04-22 23:47:27 -0400

    made a backtracking invariant function (kinda)

--- converting NDFA to DFA/(ab U aba)'.rkt
+++ converting NDFA to DFA/(ab U aba)'.rkt
@@ -25,23 +25,75 @@ first is a AND last is b
            (contains? patt (rest w)))))
 
 ;;; ci = (ab U aba)*
-(define Q0-INV
-  (lambda (ci) (or (null? ci)
-                   (contains? '(a b) ci)
-                   (contains? '(a b a) ci)))) ;; abbbbbbbba 
+#;(define Q0-INV
+    (lambda (ci) (or (null? ci)
+                     (contains? '(a b) ci)
+                     (contains? '(a b a) ci)))) ;; abbbbbbbba
+
+; is it empty? yea? then true!
+; are the first two ab? yea? then restart the algorithm!
+; oh they're not? then dont check the rest. check the first three of the og ci!
+; so ur at the point that the first two weren't ab? well are they aba? yeah? then restart the algorithm!
+; oh they're not aba? darn. false.
+
+; is it empty? yes? true!
+; are first two ab? if yes then restart with dropped first two and add dropped to accum
+; if not ab and accum is empty then false
+; if not ab and accum is not empty then check last three of appended accum and ci and drop everything in accum
+; if it is aba then restart with dropped first three and accum
+; else false 
+
+(define (Q0-INV ci)
+  (define (helper ci accum)
+    (cond [(empty? ci) #t]
+          [(= (length ci) 1)
+           (if (equal? (take-right (append (take accum 2) ci) 3) '(a b a))
+               (helper (drop ci 1) (append accum (take ci 1)))
+               #f)]
+          [(equal? (take ci 2) '(a b))
+           (helper (drop ci 2) (append accum (take ci 2)))]
+          [(equal? (take (append (take-right accum 2) ci) 3) '(a b a))
+           (helper (drop ci 1) (append accum (take ci 1)))]
+          [else #f]))
+  (and (> (length ci) 1)
+       (helper ci '())))
+
+
+(check-expect (Q0-INV '(a b)) #t)
+(check-expect (Q0-INV '(a b a)) #t)
+(check-expect (Q0-INV '(a b a b a)) #t)
+(check-expect (Q0-INV '(a b a a b a)) #t)
+(check-expect (Q0-INV '(a b a a b)) #t)
+(check-expect (Q0-INV '(a b b)) #f)
+(check-expect (Q0-INV '(a b a a b b)) #f)
+
 
 ;;;; ci = (ab U aba)*a
 (define (Q1-INV ci)
-  (if (= (length ci) 1)
-      (equal? ci '(a))
-      (and (equal? (take ci 1) '(a))
-           (equal? (take-right ci 1) '(a))
-           (ormap (lambda (i) (equal? i 'b)) ci))))
+  (and (Q0-INV (drop-right ci 1))
+       (equal? (take-right ci 1) '(a))))
+
+(check-expect (Q1-INV '(a b)) #f)
+(check-expect (Q1-INV '(a b a)) #t)
+(check-expect (Q1-INV '(a b a b a)) #t)
+(check-expect (Q1-INV '(a b a a b a)) #t)
+(check-expect (Q1-INV '(a b b)) #f)
+(check-expect (Q1-INV '(a b a a b b)) #f)
 
 ;;; ci = (ab U aba)*ab
 (define (Q2-INV ci)
-  (and (equal? (take ci 1) '(a))
-       (equal? (take-right ci 1) '(b))))
+  (and (Q0-INV (drop-right ci 1))
+       (equal? (take-right ci 2) '(a b))))
+
+
+(check-expect (Q2-INV '(a b)) #f)
+(check-expect (Q2-INV '(a b a b)) #t)
+(check-expect (Q2-INV '(a b a b a b)) #t)
+(check-expect (Q2-INV '(a b a a b a b)) #t)
+(check-expect (Q2-INV '(a b b)) #f)
+(check-expect (Q2-INV '(a b a a b b)) #f)
+
+
 
 ;accepts all inputs that only contain the pattern ab and/or aba
 (define abUaba*
@@ -66,7 +118,7 @@ first is a AND last is b
                 (Q2 ,EMP Q0))
               'no-dead))
 
-(sm-testequiv? abUaba* abUaba*2)
+;(sm-testequiv? abUaba* abUaba*2)
 
 (check-expect (sm-apply abUaba* '()) 'accept)
 (check-expect (sm-apply abUaba* '(a)) 'reject)
@@ -78,6 +130,7 @@ first is a AND last is b
 (check-expect (sm-apply abUaba* '(a b a b a)) 'accept)
 (check-expect (sm-apply abUaba* '(a b a a b a b a b a a b)) 'accept)
 
+(sm-visualize abUaba* (list 'Q0 Q0-INV) (list 'Q1 Q1-INV) (list 'Q2 Q2-INV))
 
 #|
 
@@ -95,25 +148,25 @@ C           A              B
 
 ;;INVARIANTS
 
-(define S-INV Q0-INV)
+;(define S-INV Q0-INV)
 
-(define A-INV Q1-INV)
+;(define A-INV Q1-INV)
 
-(define (B-INV ci)
-  (or (Q0-INV ci)
-      (Q2-INV ci)))
+#;(define (B-INV ci)
+    (or (Q0-INV ci)
+        (Q2-INV ci)))
 
-(define (C-INV ci)
-  (or (Q0-INV ci)
-      (Q1-INV ci)))
+#;(define (C-INV ci)
+    (or (Q0-INV ci)
+        (Q1-INV ci)))
 
 ; new theory: DS implies ci is not in language (idk how to write that)
 ; a implies b -> (or (not a) b)
-(define (DS-INV ci)
-  (and (not (S-INV ci))
-       (not (A-INV ci))
-       (not (B-INV ci))
-       (not (C-INV))))
+#;(define (DS-INV ci)
+    (and (not (S-INV ci))
+         (not (A-INV ci))
+         (not (B-INV ci))
+         (not (C-INV))))
   
 
 (define DFA
@@ -127,8 +180,8 @@ C           A              B
               (C a A)
               (C b B))))
 
-(sm-testequiv? abUaba* DFA)                  ;;why are they returning that
-(sm-testequiv? abUaba*2 DFA)
+;(sm-testequiv? abUaba* DFA)                  
+;(sm-testequiv? abUaba*2 DFA)
 
 
 (check-expect (sm-apply DFA '()) 'accept)
