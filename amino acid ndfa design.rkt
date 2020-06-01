31a944b39bd0063cbffe02635f18c1a173a4cde4
Isabella C Felix <felixisa@shu.edu>
2020-05-31 21:58:47 -0400

    updates to ndfa testing

--- amino acid ndfa design.rkt
+++ amino acid ndfa design.rkt
@@ -4,7 +4,8 @@
 ;;f is used to represent any amino acid except m 
 
 ; STEP 1
-; name: detect-motif      Σ: ((f*g)Um)a?dt^+aa(kUsUt)a*
+; name: detect-motif      Σ: (a, d, f, g, k, m, n, s, t, q)
+
 ; STEP 2
 (check-expect (sm-apply detect-motif '()) 'reject)
 (check-expect (sm-apply detect-motif '(m d t t t a a q k)) 'accept)
@@ -15,84 +16,106 @@
 (check-expect (sm-apply detect-motif '(f f f m a d t a a m k)) 'reject)
 (check-expect (sm-apply detect-motif '(f g a d t a a m k)) 'accept)
 (check-expect (sm-apply detect-motif '(m a d t a a m k)) 'accept)
+
 ; STEP 3
 #|
-Q0: nothing consumed
+Q0: nothing consumed [START]
 
-Q1: ci=(f*g)Um
+Q1: ci=(f*g) U m
 
-Q2: ci=((f*g)Um)a?
+Q2: ci=((f*g) U m) U ((f*g) U m)a
 
-Q3: ci=((f*g)Um)a?dt*
+Q3: ci=(((f*g) U m) U ((f*g) U m)a)dt*
 
-Q4: ci=((f*g)Um)a?dt^+
+Q4: ci=(((f*g) U m) U ((f*g) U m)a)dt^+
 
-Q5: ci=((f*g)Um)a?dt^+a
+Q5: ci=(((f*g) U m) U ((f*g) U m)a)dt^+a
 
-Q6: ci=((f*g)Um)a?dt^+aa
+Q6: ci=(((f*g) U m) U ((f*g) U m)a)dt^+aa
 
-Q7: ci=((f*g)Um)a?dt^+aa(mUnUq)
+Q7: ci=(((f*g) U m) U ((f*g) U m)a)dt^+aa(m U n U q)
 
-Q8: ci=((f*g)Um)a?dt^+aa(kUsUt)a*
+Q8: ci=(((f*g) U m) U ((f*g) U m)a)dt^+aa(m U n U q)(k U s U t)a* [FINAL]
 
 Q9: ci=f*
 |#
+
 ; STEP 4
+; word -> boolean
 (define (Q0-INV ci)
   (empty? ci))
 
+; word -> boolean
 (define (Q1-INV ci)
-  (if (= (length ci) 1)
-      (eq? (first ci) 'm)
-      (and (andmap (λ (i) (eq? i 'f)) (take ci (sub1 (length ci))))
+  (or (and (= (length ci) 1)
+           (eq? (first ci) 'm))
+      (and (>= (length ci) 1)
+           (andmap (λ (i) (eq? i 'f)) (take ci (sub1 (length ci))))
            (eq? (last ci) 'g))))
 
+; word -> boolean
 (define (Q2-INV ci)
-  (and (Q1-INV (takef ci (λ (i) (not (eq? i 'a)))))
+  (and (>= (length ci) 1)
+       (Q1-INV (takef ci (λ (i) (not (eq? i 'a)))))
        (or (eq? (last ci) 'g)
            (eq? (last ci) 'm)
            (eq? (last ci) 'a))))
 
+; word -> boolean
 (define (Q3-INV ci)
-  (and (Q2-INV (takef ci (λ (i) (not (eq? i 'd)))))
-       (not (empty? (dropf ci (λ (i) (not (eq? i 'd))))))
+  (and (>= (length ci) 2)
+       (Q2-INV (takef ci (λ (i) (not (eq? i 'd)))))
        (andmap (λ (i) (eq? i 't)) (rest (dropf ci (λ (i) (not (eq? i 'd)))))))) 
 
+; word -> boolean
 (define (Q4-INV ci)
-  (and (Q3-INV (takef ci (λ (i) (not (eq? i 't)))))
-       (not (empty? (takef-right ci (λ (i) (eq? i 't)))))))
+  (and (>= (length ci) 3)
+       (Q3-INV (drop-right ci 1))
+       (eq? (last ci) 't)))
 
+; word -> boolean
 (define (Q5-INV ci)
-  (and (Q4-INV (take ci (sub1 (length ci))))
+  (and (>= (length ci) 4)
+       (Q4-INV (drop-right ci 1))
        (eq? (last ci) 'a)))
 
+; word -> boolean
 (define (Q6-INV ci)
-  (and (Q5-INV (take ci (sub1 (length ci))))
+  (and (>= (length ci) 5)
+       (Q5-INV (drop-right ci 1))
        (eq? (last ci) 'a)))
 
-(define (mUnUq? w)
-  (or (eq? w 'm)
-      (eq? w 'n)
-      (eq? w 'q)))
-
+; word -> boolean
 (define (Q7-INV ci)
-  (and (Q6-INV (take ci (sub1 (length ci))))
-       (mUnUq? (last ci))))
+  (and (>= (length ci) 6)
+       (Q6-INV (drop-right ci 1))
+       (not (empty? (member (last ci) '(m n q))))))
 
+; symbol -> boolean
 (define (kUsUt? w)
   (or (eq? w 'k)
       (eq? w 's)
       (eq? w 't)))
 
+; word -> boolean
 (define (Q8-INV ci)
-  (and (Q7-INV (dropf-right ci (λ (i) (or (kUsUt? i)
+  (and (>= (length ci) 7)
+       (let ((Q7-word (dropf-right ci (λ (i) (or (eq? i 'k)
+                                          (eq? i 's)
+                                          (eq? i 't)
                                           (eq? i 'a)))))
-       (not (empty? (takef-right ci (λ (i) (or (kUsUt? i)
-                                               (eq? i 'a))))))
-       (andmap (λ (i) (eq? i 'a)) (takef-right ci (λ (i) (not (kUsUt? i)))))))
+             (drop-Q7 (takef-right ci (λ (i) (or (eq? i 'k)
+                                          (eq? i 's)
+                                          (eq? i 't)
+                                          (eq? i 'a))))))
+       (and (Q7-INV Q7-word)
+            (or (eq? (first drop-Q7) 'k) (eq? (first drop-Q7) 's) (eq? (first drop-Q7) 't))
+            (andmap (λ (i) (eq? i 'a)) (rest drop-Q7))))))
 
+; word -> boolean
 (define (Q9-INV ci)
   (andmap (λ (i) (eq? i 'f)) ci))
+
 ; STEPS 5 and 6
 (define detect-motif
   (make-ndfa '(Q0 Q1 Q2 Q3 Q4 Q5 Q6 Q7 Q8 Q9)
@@ -118,18 +141,20 @@ Q9: ci=f*
                (Q8 a Q8)
                (Q9 g Q1))
              'no-dead))
+
 ; STEP 7
 #|
-E(Q0)=Q0,Q9
-E(Q1)=Q1,Q2
-E(Q2)=Q2
-E(Q3)=Q3
-E(Q4)=Q4
-E(Q5)=Q5
-E(Q6)=Q6
-E(Q7)=Q7
-E(Q8)=Q8
-E(Q9)=Q9
+State     E(State)
+Q0        Q0,Q9
+Q1        Q1,Q2
+Q2        Q2
+Q3        Q3
+Q4        Q4
+Q5        Q5
+Q6        Q6
+Q7        Q7
+Q8        Q8
+Q9        Q9
 |#
 ; STEP 8
 #|
@@ -137,8 +162,9 @@ Proof by induction on |w| that INVs always hold
 
 Base |w| = 0
 |w| = 0 ==> w = Ɛ
-We must show that INVs hold for states in E(Q0) = Q0
-|w| = Ɛ ==> Q0-INV
+We must show that INVs hold for states in E(Q0) = Q0,Q9
+w = Ɛ ==> Q0-INV
+w = Ɛ ==> Q9-INV
 
 Inductive Step
 Assume: INVs hold for |w| = k
@@ -149,12 +175,6 @@ For consuming v the INVs hold by IH
 Must show for (p i q) that INVs for states in E(q) hold given p-INV
 holds and i is consumed:
 
-Q0-INV && (Q0 Ɛ Q9)
-Q0-INV ==> v=empty
-       ==> i=Ɛ
-       ==> ci=f*
-       ==> Q9-INV 
-
 Q0-INV && (Q0 m Q1)
 Q0-INV ==> v=empty
        ==> i=m
@@ -162,114 +182,126 @@ Q0-INV ==> v=empty
        ==> ci=(f*g)Um
        ==> Q1-INV
 
-    ci=m ==> i=Ɛ
-         ==> ci=((f*g)Um)a?
-         ==> Q2-INV
+ci=(f*g)Um ==> ci=((f*g) U m) U ((f*g) U m)a
+           ==> Q2-INV
 
 Q1-INV && (Q1 a Q2)
 Q1-INV ==> v=(f*g)Um
        ==> i=a
-       ==> ci=((f*g)Um)a?
+       ==> ci=((f*g) U m)a
+       ==> ci=((f*g) U m) U ((f*g) U m)a
        ==> Q2-INV
 
 Q2-INV && (Q2 d Q3)
-Q2-INV ==> v=((f*g)Um)a?
+Q2-INV ==> v=((f*g) U m) U ((f*g) U m)a
        ==> i=d
-       ==> ci=((f*g)Um)a?dt*
+       ==> ci=(((f*g) U m) U ((f*g) U m)a)d
+       ==> ci=(((f*g) U m) U ((f*g) U m)a)dt*
        ==> Q3-INV
 
 Q3-INV && (Q3 t Q3)
-Q3-INV ==> v=((f*g)Um)a?dt*
+Q3-INV ==> v=(((f*g) U m) U ((f*g) U m)a)dt*
        ==> i=t
-       ==> ci=((f*g)Um)a?dt*
+       ==> ci=(((f*g) U m) U ((f*g) U m)a)dt*t
+       ==> ci=(((f*g) U m) U ((f*g) U m)a)dt*
        ==> Q3-INV
 
 Q3-INV && (Q3 t Q4)
-Q3-INV ==> v=((f*g)Um)a?dt*
+Q3-INV ==> v=(((f*g) U m) U ((f*g) U m)a)dt*
        ==> i=t
-       ==> ci=((f*g)Um)a?dt^+
+       ==> ci=(((f*g) U m) U ((f*g) U m)a)dt*t
+       ==> ci=(((f*g) U m) U ((f*g) U m)a)dt^+
        ==> Q4-INV
 
 Q4-INV && (Q4 a Q5)
-Q4-INV ==> v=((f*g)Um)a?dt^+
+Q4-INV ==> v=(((f*g) U m) U ((f*g) U m)a)dt^+
        ==> i=a
-       ==> ci=((f*g)Um)a?dt^+a
+       ==> ci=(((f*g) U m) U ((f*g) U m)a)dt^+a
        ==> Q5-INV
 
 Q5-INV && (Q5 a Q6)
-Q5-INV ==> v=((f*g)Um)a?dt^+a
+Q5-INV ==> v=(((f*g) U m) U ((f*g) U m)a)dt^+a
        ==> i=a
-       ==> ci=((f*g)Um)a?dt^+aa
+       ==> ci=(((f*g) U m) U ((f*g) U m)a)dt^+aa
        ==> Q6-INV
 
 Q6-INV && (Q6 m Q7)
-Q6-INV ==> v=((f*g)Um)a?dt^+aa
+Q6-INV ==> v=(((f*g) U m) U ((f*g) U m)a)dt^+aa
        ==> i=m
-       ==> ci=((f*g)Um)a?dt^+aa(mUnUq)
+       ==> ci=(((f*g) U m) U ((f*g) U m)a)dt^+m
+       ==> ci=(((f*g) U m) U ((f*g) U m)a)dt^+aa(m U n U q)
        ==> Q7-INV
 
 Q6-INV && (Q6 n Q7)
-Q6-INV ==> v=((f*g)Um)a?dt^+aa
+Q6-INV ==> v=(((f*g) U m) U ((f*g) U m)a)dt^+aa
        ==> i=n
-       ==> ci=((f*g)Um)a?dt^+aa(mUnUq)
+       ==> ci=(((f*g) U m) U ((f*g) U m)a)dt^+aan
+       ==> ci=(((f*g) U m) U ((f*g) U m)a)dt^+aa(m U n U q)
        ==> Q7-INV
 
 Q6-INV && (Q6 q Q7)
-Q6-INV ==> v=((f*g)Um)a?dt^+aa
+Q6-INV ==> v=(((f*g) U m) U ((f*g) U m)a)dt^+aa
        ==> i=q
-       ==> ci=((f*g)Um)a?dt^+aa(mUnUq)
+       ==> ci=(((f*g) U m) U ((f*g) U m)a)dt^+aaq
+       ==> ci=(((f*g) U m) U ((f*g) U m)a)dt^+aa(m U n U q)
        ==> Q7-INV
 
 Q7-INV && (Q7 k Q8)
-Q7-INV ==> v=((f*g)Um)a?dt^+aa(mUnUq)
+Q7-INV ==> v=(((f*g) U m) U ((f*g) U m)a)dt^+aa(m U n U q)
        ==> i=k
-       ==> ci=((f*g)Um)a?dt^+aa(mUnUq)(kUsUt)a*
+       ==> ci=(((f*g) U m) U ((f*g) U m)a)dt^+aa(m U n U q)k
+       ==> ci=(((f*g) U m) U ((f*g) U m)a)dt^+aa(m U n U q)(k U s U t)a*
        ==> Q8-INV
 
 Q7-INV && (Q7 s Q8)
-Q7-INV ==> v=((f*g)Um)a?dt^+aa(mUnUq)
+Q7-INV ==> v=(((f*g) U m) U ((f*g) U m)a)dt^+aa(m U n U q)
        ==> i=s
+       ==> ci=(((f*g) U m) U ((f*g) U m)a)dt^+aa(m U n U q)s
        ==> ci=((f*g)Um)a?dt^+aa(mUnUq)(kUsUt)a*
        ==> Q8-INV
 
 Q7-INV && (Q7 t Q8)
-Q7-INV ==> v=((f*g)Um)a?dt^+aa(mUnUq)
+Q7-INV ==> v=(((f*g) U m) U ((f*g) U m)a)dt^+aa(m U n U q)
        ==> i=t
-       ==> ci=((f*g)Um)a?dt^+aa(mUnUq)(kUsUt)a*
+       ==> ci=(((f*g) U m) U ((f*g) U m)a)dt^+aa(m U n U q)t
+       ==> ci=(((f*g) U m) U ((f*g) U m)a)dt^+aa(m U n U q)(k U s U t)a*
        ==> Q8-INV
 
 Q8-INV && (Q8 a Q8)
-Q8-INV ==> v=((f*g)Um)a?dt^+aa(mUnUq)(kUsUt)a*
+Q8-INV ==> v=(((f*g) U m) U ((f*g) U m)a)dt^+aa(m U n U q)(k U s U t)a*
        ==> i=a
-       ==> ci=((f*g)Um)a?dt^+aa(mUnUq)(kUsUt)a*
+       ==> ci=(((f*g) U m) U ((f*g) U m)a)dt^+aa(m U n U q)(k U s U t)a*a
+       ==> ci=(((f*g) U m) U ((f*g) U m)a)dt^+aa(m U n U q)(k U s U t)a*
        ==> Q8-INV
 
 Q9-INV && (Q9 f Q9)
 Q9-INV ==> v=f*
        ==> i=f
+       ==> ci=f*f
        ==> ci=f*
        ==> Q9-INV
 
 Q9-INV && (Q9 g Q1)
-Q9-INV ==> v= empty OR f^+
+Q9-INV ==> v=f*
        ==> i=g
+       ==> ci=f*g
        ==> ci=(f*g)Um
        ==> Q1-INV
 
 Q.E.D.
 
 
-Prove: L(M) <==> L = ((f*g)Um)a?dt^+aa(mUnUq)(kUsUt)a*
+Prove: L(M) <==> L = (((f*g) U m) U ((f*g) U m)a)dt^+aa(m U n U q)(k U s U t)a*
 
 1. w in L(M) <==> w in L
 
    w in L(M) ==> M ends in Q8
-   INVs always hold ==> w = ((f*g)Um)a?dt^+aa(mUnUq)(kUsUt)a*
+   INVs always hold ==> w = (((f*g) U m) U ((f*g) U m)a)dt^+aa(m U n U q)(k U s U t)a*
                     ==> w in L
 
 
    w in L ==> w in L(M)
-   w in L ==> w = ((f*g)Um)a?dt^+aa(mUnUq)(kUsUt)a*
+   w in L ==> w = (((f*g) U m) U ((f*g) U m)a)dt^+aa(m U n U q)(k U s U t)a*
    INVs always hold ==> M ends in Q8
                     ==> w in L(M)
 
@@ -280,7 +312,7 @@ Prove: L(M) <==> L = ((f*g)Um)a?dt^+aa(mUnUq)(kUsUt)a*
 
 
    w not in L ==> w not in L(M)
-   w not in L ==> w != ((f*g)Um)a?dt^+aa(mUnUq)(kUsUt)a*
+   w not in L ==> w != (((f*g) U m) U ((f*g) U m)a)dt^+aa(m U n U q)(k U s U t)a*
    INVs always hold ==> M does not end in Q8
                     ==> w not in L(M)
    
@@ -288,5 +320,16 @@ Q.E.D.
 |#
 
 (sm-graph detect-motif)
+(sm-visualize detect-motif
+              (list 'Q0 Q0-INV)
+              (list 'Q1 Q1-INV)
+              (list 'Q2 Q2-INV)
+              (list 'Q3 Q3-INV)
+              (list 'Q4 Q4-INV)
+              (list 'Q5 Q5-INV)
+              (list 'Q6 Q6-INV)
+              (list 'Q7 Q7-INV)
+              (list 'Q8 Q8-INV)
+              (list 'Q9 Q9-INV))
 
 (test)
--- ndfa-testing.rkt
+++ ndfa-testing.rkt
@@ -1,63 +1,140 @@
 #lang racket
-(require fsm test-engine/racket-tests "dfas.rkt" "ndfas.rkt" "pdas.rkt")
-(provide test-inputs
-         new-inputs
-         remove-EMP) 
+(require fsm test-engine/racket-tests "dfas.rkt" "ndfas.rkt" "pdas.rkt") 
 
 ; test-inputs: machine -> slist
 ; Purpose: To generate the least amount of input words that test every node of a machine  
 (define (test-inputs m)
-  ; helper: machine test-words unfinished-words expanded-states
-  ; Purpose: To generate the least amount of input words that test every node of a machine 
-  ; ACCUM-INVS
-  ; t-words: tested words thus far
-  ; u-words: unfinished words thus far
-  ; e-states: expanded states thus far 
-  (define (helper t-words u-words e-states)
+
+  ; find-all-trans: state -> (listof rules) 
+  ; Purpose: To find all transitions from the given current state
+  (define (find-all-trans st)
+    (filter (lambda (x) (equal? st (car x))) (sm-getrules m)))
+    
+  ; helper: test-words expanded-transitions unfinished-words expanded-states
+  ; Purpose: To generate the least amount of input words that test every edge of a machine 
+  (define (helper t-words e-trans u-words e-states)
 
     ; new-u-words: slist -> slist
     ; Purpose: To update u-words accumulator
     (define (new-u-words u-words)
-    
-      ; state (listof rules) -> (listof rules) 
-      ; finds all transitions with the given current state
-      (define (find-all-trans st)
-        (filter (lambda (x) (equal? st (car x))) (sm-getrules m)))
-    
       (cond [(member (caar u-words) e-states) (cdr u-words)]
             [else (map (lambda (rule) (list (caddr rule)
                                             (append (cadar u-words) (list (cadr rule)))))
                        (find-all-trans (caar u-words)))]))
     
+    ; new-e-states: (listof state) -> (listof state)
+    ; Purpose: To update e-states accumulator
+    (define (new-e-states st)
+
+      ; are-these-in-there?: list list -> boolean
+      ; Purpose: Determines if all the contents of L1 are in L2 
+      (define (are-these-in-there? L1 L2)
+        (andmap (lambda (i) (not (false? (member i L2)))) L1))
+
+      (if (are-these-in-there? (find-all-trans st) e-trans)
+          (cons st e-states)
+          e-states))
+    
     (cond [(null? u-words) t-words]
-          [(member (caar u-words) e-states) (helper (cons (cadar u-words) t-words) (cdr u-words) e-states)]
+          [(member (caar u-words) e-states) (helper (cons (cadar u-words) t-words) e-trans (cdr u-words) e-states)]
           [else (helper (cons (cadar u-words) t-words)
+                        (append (find-all-trans (caar u-words)) e-trans)
                         (append (cdr u-words) (new-u-words u-words))
-                        (cons (caar u-words) e-states))]))
+                        (new-e-states (caar u-words)))]))
 
-  (reverse (helper '() (list (list (sm-getstart m) '())) '())))
-          
-; new-inputs: slist -> slist
-; Purpose: To remove input words that are substrings of other input words in the list
-;          i.e. remove '(a) because it is a substring of '(a a) and '(a b a)
-(define (new-inputs loi)
+  (reverse (helper '() (find-all-trans (sm-getstart m)) (list (list (sm-getstart m) '())) '())))
+
+
+
+
+; remove-bad: (listof word) -> (listof word)
+; Purpose: To remove words that test transitions more than necessary
+(define (remove-bad loi m)
+
+  ; get-path: word -> (listof state)
+  ; Purpose: To extract the states in sm-showtransitions
+  (define (get-path i)
+    (map (λ (x) (cadr x)) (drop-right (sm-showtransitions m i) 1)))
   (cond [(null? loi) '()]
-        [(null? (car loi)) (cons '() (new-inputs (cdr loi)))]
-        [(not (ormap (lambda (i) (equal? (car loi) (take i (length (car loi))))) (cdr loi)))
-         (cons (car loi) (new-inputs (cdr loi)))]
-        [else (new-inputs (cdr loi))]))
+        [(null? (car loi)) (cons '() (remove-bad (cdr loi) m))]
+        [(not (ormap (lambda (i) (equal? (get-path (car loi)) (take i (length (get-path (car loi)))))) (map (λ (x) (get-path x)) (cdr loi))))
+         (cons (car loi) (remove-bad (cdr loi) m))]
+        [else (remove-bad (cdr loi) m)]))
 
-(check-expect (new-inputs '(() (a) (b) (a a) (a b) (b a) (b b) (a b a) (a b b)))
-              '(() (a a) (b a) (b b) (a b a) (a b b)))
-(check-expect (new-inputs (test-inputs a-bc*-d))
-              '(() (c) (d) (a b) (a c) (a a) (b a) (b b) (b c) (b d) (a d a) (a d b) (a d c) (a d d)))
+(check-expect (remove-bad '(() (c) (b b) (b c) (a c) (a a) (a b)) AT-LEAST-ONE-MISSING) '(() (b c) (a c) (a b)))
 
-; remove-EMP: (listof inputs) -> (listof inputs)
-; Purpose: To remove e from test inputs
-(define (remove-EMP loi)
-  (map (λ (x) (if (member 'e x) (remove 'e x) x)) loi))
 
-(check-expect (remove-EMP '(() (a e) (a b e))) '(() (a) (a b)))
-(check-expect (remove-EMP '(() (a) (b) (a b e))) '(() (a) (b) (a b)))
+
+; ndfa-test: machine -> (listof word)
+; Purpose: generate ndfa test strings 
+(define (ndfa-tests m)
+
+  ; remove-bad: (listof word) -> (listof word)
+  ; Purpose: To remove words that test transitions more than necessary
+  (define (remove-bad loi)
+
+    ; get-path: word -> (listof state)
+    ; Purpose: To extract onle the states in sm-showtransitions
+    (define (get-path i)
+      (map (λ (x) (cadr x)) (drop-right (sm-showtransitions m i) 1)))
+    
+    (cond [(null? loi) '()]
+          [(null? (car loi)) (cons '() (remove-bad (cdr loi)))]
+          [(symbol? (sm-showtransitions m (car loi))) (cons (car loi) (remove-bad (cdr loi)))]
+          [(not (ormap (lambda (i) (equal? (get-path (car loi)) (take i (length (get-path (car loi)))))) (map (λ (x) (get-path x)) (cdr loi))))
+           (cons (car loi) (remove-bad (cdr loi)))]
+          [else (remove-bad (cdr loi))]))
+
+  ; remove-EMP: (listof inputs) -> (listof inputs)
+  ; Purpose: To remove e from test inputs
+  (define (remove-EMP loi)
+    (map (λ (x) (filter (λ (i) (not (eq? i 'ε))) x)) loi))
+
+  ; new-inputs: slist -> slist
+  ; Purpose: To remove input words that are substrings of other input words in the list
+  ;          i.e. remove '(a) because it is a substring of '(a a) and '(a b a)
+  (define (new-inputs loi)
+    (cond [(null? loi) '()]
+          [(null? (car loi)) (cons '() (new-inputs (cdr loi)))]
+          [(not (ormap (lambda (i) (equal? (car loi) (take i (length (car loi))))) (cdr loi)))
+           (cons (car loi) (new-inputs (cdr loi)))]
+          [else (new-inputs (cdr loi))]))
+  
+  (remove-EMP
+   (remove-bad
+    (new-inputs (test-inputs m)))
+  )
+  )
+
+(define detect-motif
+  (make-ndfa '(Q0 Q1 Q2 Q3 Q4 Q5 Q6 Q7 Q8 Q9)
+             '(a d g f k m n q s t)
+             'Q0
+             '(Q8)
+             `((Q0 ,EMP Q9)
+               (Q0 m Q1)
+               (Q9 f Q9)
+               (Q1 a Q2)
+               (Q1 ,EMP Q2)
+               (Q2 d Q3)
+               (Q3 t Q4)
+               (Q3 t Q3)
+               (Q4 a Q5)
+               (Q5 a Q6)
+               (Q6 m Q7)
+               (Q6 n Q7)
+               (Q6 q Q7)
+               (Q7 k Q8)
+               (Q7 s Q8)
+               (Q7 t Q8)
+               (Q8 a Q8)
+               (Q9 g Q1))
+             'no-dead))
+
+ ; remove-EMP: (listof inputs) -> (listof inputs)
+  ; Purpose: To remove e from test inputs
+  (define (remove-EMP loi)
+    (map (λ (x) (if (member 'ε x) (remove 'ε x) x)) loi))
+
 
 (test)
